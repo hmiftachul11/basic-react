b8e15544b6773b56d8cc62fd52e7e7b39e1c2700
hmiftachul11 <hmiftachul11@gmail.com>
2023-12-09 02:06:33 +0700

    menjumlahkan bil. bulat

---
+++ soal-ifelse/forloop-bilbul.js
@@ -0,0 +1,9 @@
+// Implementasikan sebuah program JavaScript yang menggunakan loop for untuk menghitung jumlah semua bilangan bulat dari 1 hingga 5.
+
+let total = 0
+
+for(let i = 0; i <= 5; i++) {
+    total+= i
+}
+
+console.log(total);
